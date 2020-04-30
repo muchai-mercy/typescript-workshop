d1edf7734920c4ec10df642ff89984300e7e2241
Mercy Muchai <muchaimercyw@gmail.com>
2020-04-30 16:17:33 +0300

    helloworld

---
+++ helloWorld/helloWorld.js
@@ -0,0 +1,4 @@
+var hello = function (name, age) {
+    console.log("Hello World, my name is " + name + ", I'm " + age + "years old!");
+};
+hello("Muchai", 10);
---
+++ helloWorld/helloWorld.ts
@@ -0,0 +1,5 @@
+const hello = (name: string, age: number) => {
+  console.log(`Hello World, my name is ${name}, I'm ${age}years old!`);
+}
+
+hello("Muchai", 10);
